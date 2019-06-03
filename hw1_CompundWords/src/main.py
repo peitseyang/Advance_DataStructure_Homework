8b883c5ddac1e7110491c6448b2a59295269ccab
AlexYang <alex10130010@gmail.com>
2019-06-03 22:52:06 +0800

    hw1 coding finished

---
+++ hw1_CompundWords/src/main.py
@@ -0,0 +1,79 @@
+import sys
+
+class TrieNode: 
+    def __init__(self): 
+        self.child = [None]*26
+        self.isEndOfWord = False
+  
+class Trie: 
+    def __init__(self): 
+        self.root = self.getNode() 
+  
+    def getNode(self): 
+        return TrieNode() 
+  
+    def char2Index(self, ch): 
+        return ord(ch)-ord('a') 
+  
+    def insert(self, key):
+        leaf = self.root
+        for i in range(len(key)):
+            idx = self.char2Index(key[i])
+            if not leaf.child[idx]: 
+                leaf.child[idx] = self.getNode() 
+            leaf = leaf.child[idx] 
+        leaf.isEndOfWord = True
+  
+    def search(self, key):
+        leaf = self.root
+        for i in range(len(key)): 
+            idx = self.char2Index(key[i]) 
+            if not leaf.child[idx]: 
+                return False
+            leaf = leaf.child[idx]
+        return leaf != None and leaf.isEndOfWord
+
+if __name__ == '__main__':
+    # dir
+    input_dir = './data/Compound Words.in'
+    output_dir = './data/Compound Words.out'
+
+    # create Trie
+    dictionary = Trie()
+
+    # insert words
+    for line in open(input_dir, 'r').readlines():
+        key = line.strip()
+        if key == 'April' or key == 'December' or key == 'February' or key == 'Friday':
+            continue
+        dictionary.insert(key)
+    
+    # doc
+    result = []
+
+    # search Compound Words
+    for line in open(input_dir, 'r').readlines():
+        word = line.strip()
+        if word == 'April' or word == 'December' or word == 'February' or word == 'Friday':
+            continue
+        for i in range(1, len(word)):
+            sub_first = word[0:i]
+            sub_second = word[i:len(word)]
+            if dictionary.search(sub_first) and dictionary.search(sub_second):
+                result.append(word)
+                break
+
+    # check accuracy
+    ground_truth = open(output_dir, 'r').readlines()
+    if len(result) != len(ground_truth):
+        print('Result count inconsist')
+    else:
+        count = 0
+        for i in range(len(result)):
+            word = ground_truth[i].strip()
+            if result[i] == word:
+                count += 1
+        if count == len(result):
+            print('Match output file')
+        else:
+            print('Some error in result')
